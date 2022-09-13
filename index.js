958e5c135838a47c4f6bc1fc58d2d0ad03a4671e
Ninadjoshi212 <ninadjoshi212@gmail.com>
2022-09-14 00:18:53 +0530

    js added

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 window.addEventListener("scroll", function () {
-    const header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//navigation sidebar menu
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+const navigationItems = document.querySelectorAll(".navigation a");
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+//navigation sidebar menu item click
+navigationItems.forEach((navigationItem) => {
+  navigationItem.addEventListener("click", () => {
+    menuBtn.classList.remove("active");
+    navigation.classList.remove("active");
   });
+});
+
+ //reveal elements on scroll from top
+ window.addEventListener("scroll", reveal);
   
-  //navigation sidebar menu
-  const menuBtn = document.querySelector(".menu-btn");
-  const navigation = document.querySelector(".navigation");
-  const navigationItems = document.querySelectorAll(".navigation a");
-  
-  menuBtn.addEventListener("click", () => {
-    menuBtn.classList.toggle("active");
-    navigation.classList.toggle("active");
-  });
-  
-  //navigation sidebar menu click
-  navigationItems.forEach((navigationItem) => {
-    navigationItem.addEventListener("click", () => {
-      menuBtn.classList.remove("active");
-      navigation.classList.remove("active");
-    });
-  });
-  
+ function reveal(){
+   var reveals = document.querySelectorAll(".reveal");
+ 
+   for(var i = 0; i < reveals.length; i++){
+     var windowHeight = window.innerHeight;
+     var revealTop = reveals[i].getBoundingClientRect().top;
+     var revealPoint = 50;
+ 
+     if(revealTop < windowHeight - revealPoint){
+       reveals[i].classList.add("active");
+     }
+     else if(revealTop > windowHeight - revealPoint){
+      reveals[i].classList.remove("active");
+     }
+   }
+ }
